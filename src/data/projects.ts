8d4642f0d5db4918836bffef29ec9a22fbae447d
ChiragDalmia <119760552+ChiragDalmia@users.noreply.github.com>
2024-08-18 01:16:52 -0400

    fixed tailwind config

---
+++ src/data/projects.ts
@@ -0,0 +1,244 @@
+// src/data/projects.ts
+
+export const projects = [
+  {
+    title: "Weather App",
+    description: "Real-time weather information",
+    icon: "/weatherapp.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/01-weatherApp/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/01-weatherApp/",
+  },
+  {
+    title: "Task Manager",
+    description: "Task management application",
+    icon: "/todoList.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/02-todoList/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/02-todoList/",
+  },
+  {
+    title: "Stop Watch",
+    description: "Time tracking tool",
+    icon: "/stopwatch.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/03-stopWatch/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/03-stopWatch/",
+  },
+  {
+    title: "Rock Paper Scissor",
+    description: "Classic game implementation",
+    icon: "/rockpaperscissor.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/04-rockPaperScissor/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/04-rockPaperScissor/",
+  },
+  {
+    title: "Quiz App",
+    description: "Interactive quiz application",
+    icon: "/quizapp.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/05-quizApp/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/05-quizApp/",
+  },
+  {
+    title: "Password Generator",
+    description: "Secure password creation tool",
+    icon: "/passwordgenerator.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/06-passwordGenerator/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/06-passwordGenerator/",
+  },
+  {
+    title: "Notes App",
+    description: "Digital note-taking application",
+    icon: "/notesapp.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/07-notesApp/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/07-notesApp/",
+  },
+  {
+    title: "Age Calculator",
+    description: "Calculate age from birthdate",
+    icon: "/agecalculator.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/08-ageCalculator/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/08-ageCalculator/",
+  },
+  {
+    title: "Quote Generator",
+    description: "Random quote display",
+    icon: "/quotegenerator.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/09-quoteGenerator/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/09-quoteGenerator/",
+  },
+  {
+    title: "QR Generator",
+    description: "Create QR codes easily",
+    icon: "/qrgenerator.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/10-qrGenerator/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/10-qrGenerator/",
+  },
+  {
+    title: "Music Player",
+    description: "Audio playback application",
+    icon: "/musicplayer.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/11-musicPlayer/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/11-musicPlayer/",
+  },
+  {
+    title: "Calculator",
+    description: "Basic arithmetic operations",
+    icon: "/calculator.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/12-calculator/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/12-calculator/",
+  },
+  {
+    title: "Toggle Password",
+    description: "Password visibility toggle",
+    icon: "/togglepassword.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/13-togglePassword/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/13-togglePasssword/",
+  },
+  {
+    title: "Form Validation",
+    description: "Input validation demo",
+    icon: "/formvalidation.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/14-formValidation/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/14-formValidation/",
+  },
+  {
+    title: "Image Gallery",
+    description: "Dynamic image showcase",
+    icon: "/imagegallery.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/15-imageGallery/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/15-imageGallery/",
+  },
+  {
+    title: "Toast Notification",
+    description: "Custom alert messages",
+    icon: "/toastnotification.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/16-toastNotification/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/16-toastNotification/",
+  },
+  {
+    title: "Popup",
+    description: "Modal window implementation",
+    icon: "/popup.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/17-popup/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/17-popup/",
+  },
+  {
+    title: "Clock App",
+    description: "Digital clock display",
+    icon: "/clockapp.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/18-clockApp/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/18-clockApp/",
+  },
+  {
+    title: "Password Strength",
+    description: "Password security checker",
+    icon: "/passwordstrength.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/19-passwordStrength/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/19-passwordStrength/",
+  },
+  {
+    title: "Translator App",
+    description: "Language translation tool",
+    icon: "/translatorapp.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/20-translatorApp/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/20-translatorApp/",
+  },
+  {
+    title: "Progress Bar",
+    description: "Visual progress indicator",
+    icon: "/progressbar.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/21-progressBar/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/21-progressBar/",
+  },
+  {
+    title: "Timer",
+    description: "Countdown timer application",
+    icon: "/timer.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/22-timer/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/22-timer/",
+  },
+  {
+    title: "Drag & Drop",
+    description: "Interactive element moving",
+    icon: "/drag&drop.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/23-drag&drop/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/23-drag&drop/",
+  },
+  {
+    title: "Image Search",
+    description: "Visual content finder",
+    icon: "/imagesearch.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/24-imageSearch/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/24-imageSearch/",
+  },
+  {
+    title: "Crypto Wallet",
+    description: "Digital currency manager",
+    icon: "/cryptowallet.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/25-cryptoWallet/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/25-cryptoWallet/",
+  },
+  {
+    title: "Endless Runner",
+    description: "Infinite scrolling game",
+    icon: "/endlessrunner.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/26-endlessRunner/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/26-endlessRunner/",
+  },
+  {
+    title: "Tic Tac Toe",
+    description: "Classic game implementation",
+    icon: "/tictactoe.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/27-tictactoe/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/27-tictactoe/",
+  },
+  {
+    title: "Dictionary",
+    description: "Word lookup tool",
+    icon: "/dictionary.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/28-dictionary/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/28-dictionary/",
+  },
+  {
+    title: "Currency Convertor",
+    description: "Exchange rate calculator",
+    icon: "/currencyconvertor.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/29-currencyConvertor/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/29-currencyConvertor/",
+  },
+  {
+    title: "Loan Calculator",
+    description: "Financial planning tool",
+    icon: "/loancalculator.png",
+    github:
+      "https://github.com/ChiragDalmia/projects/tree/main/30-javascript-projects/30-loanCalculator/",
+    demo: "https://projects-chirag-dalmia.vercel.app/30-javascript-projects/30-loanCalculator/",
+  },
+];
